6f01b89536b7c2440aa72c6d9a86ed0430dbe11e
Sanjay Singh Rathor <sanjayrathore36@gmail.com>
2026-05-24 14:10:32 +0530

    Initial Version

---
+++ SALiveActivity/SALiveActivity/SALiveActivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  SALiveActivityApp.swift
+//  SALiveActivity
+//
+//  Created by sanjay.rathor1 on 24/05/26.
+//
+
+import SwiftUI
+
+@main
+struct SALiveActivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SALiveActivity/SALiveActivityTests/SALiveActivityTests.swift
@@ -0,0 +1,19 @@
+//
+//  SALiveActivityTests.swift
+//  SALiveActivityTests
+//
+//  Created by sanjay.rathor1 on 24/05/26.
+//
+
+import Testing
+@testable import SALiveActivity
+
+struct SALiveActivityTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
